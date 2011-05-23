49749511386d66b026d9da60b6fb5d8948b56e8d
Matthew Scott <gldnspud@gmail.com>
2011-05-22 21:45:28 -0700

    Alphabetical order cleanup; add __unicode__ methods and proper pluralization.

--- wbinventory/models.py
+++ wbinventory/models.py
@@ -29,6 +29,12 @@ class Category(models.Model):
 
     name = models.CharField(max_length=200)
 
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __unicode__(self):
+        return self.name
+
 
 class Currency(models.Model):
     """A type of currency used in the purchase or sale of items."""
@@ -36,6 +42,12 @@ class Currency(models.Model):
     code = models.CharField(max_length=10)
     name = models.CharField(max_length=50)
     symbol = models.CharField(max_length=10)
+
+    class Meta:
+        verbose_name_plural = 'Currencies'
+
+    def __unicode__(self):
+        return self.code
 
 
 class Item(models.Model):
@@ -45,11 +57,14 @@ class Item(models.Model):
     description = models.TextField(blank=True, default='')
     default_uom = models.ForeignKey(UOM_MODEL)
     default_location = models.ForeignKey(LOCATION_MODEL)
-    reorder_quantity = quantity_field(null=True, default=None)
-    target_quantity = quantity_field(null=True, default=None)
+    reorder_quantity = quantity_field(null=True, blank=True, default='')
+    target_quantity = quantity_field(null=True, blank=True, default='')
 
     categories = models.ManyToManyField(CATEGORY_MODEL, related_name='items', through='ItemCategory')
     suppliers = models.ManyToManyField(THIRD_PARTY_MODEL, related_name='items', through='ItemSupplier')
+
+    def __unicode__(self):
+        return self.number
 
 
 class ItemCategory(models.Model):
@@ -57,6 +72,9 @@ class ItemCategory(models.Model):
 
     item = models.ForeignKey(ITEM_MODEL)
     category = models.ForeignKey(CATEGORY_MODEL)
+
+    class Meta:
+        verbose_name_plural = 'Item Categories'
 
 
 class ItemLocation(models.Model):
@@ -66,15 +84,6 @@ class ItemLocation(models.Model):
     location = models.ForeignKey(LOCATION_MODEL)
     uom = models.ForeignKey(UOM_MODEL)
     quantity = quantity_field(default='0')
-
-
-class ItemSupplier(models.Model):
-    """A third party that supplies an item."""
-
-    item = models.ForeignKey(ITEM_MODEL)
-    third_party = models.ForeignKey(THIRD_PARTY_MODEL)
-    priority = models.IntegerField(default=1)  # lower value == higher priority
-    notes = models.TextField(blank=True, default='')
 
 
 class ItemPrice(models.Model):
@@ -87,6 +96,18 @@ class ItemPrice(models.Model):
     currency = models.ForeignKey(CURRENCY_MODEL)
     price = price_field(default='0')
 
+    def __unicode__(self):
+        return u'{0} {1}'.format(self.currency.symbol, self.price)
+
+
+class ItemSupplier(models.Model):
+    """A third party that supplies an item."""
+
+    item = models.ForeignKey(ITEM_MODEL)
+    third_party = models.ForeignKey(THIRD_PARTY_MODEL)
+    priority = models.IntegerField(default=1)  # lower value == higher priority
+    notes = models.TextField(blank=True, default='')
+
 
 class Location(models.Model):
     """An area where items are kept."""
@@ -94,8 +115,11 @@ class Location(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True, default='')
     notes = models.TextField(blank=True, default='')
-    zone = models.ForeignKey('Zone', null=True)
-    third_party = models.ForeignKey(THIRD_PARTY_MODEL, null=True)
+    zone = models.ForeignKey('Zone', null=True, blank=True)
+    third_party = models.ForeignKey(THIRD_PARTY_MODEL, null=True, blank=True)
+
+    def __unicode__(self):
+        return self.name
 
 
 class ThirdParty(models.Model):
@@ -112,6 +136,12 @@ class ThirdParty(models.Model):
     is_supplier = models.BooleanField(default=False)
     is_manufacturer = models.BooleanField(default=False)
 
+    class Meta:
+        verbose_name_plural = 'Third Parties'
+
+    def __unicode__(self):
+        return self.name
+
 
 class UnitOfMeasure(models.Model):
     """A unit of measure, used descriptively and for conversion."""
@@ -119,8 +149,17 @@ class UnitOfMeasure(models.Model):
     name = models.CharField(max_length=50)
     default = models.BooleanField(default=False)
 
+    class Meta:
+        verbose_name_plural = 'Units of Measure'
+
+    def __unicode__(self):
+        return self.name
+
 
 class Zone(models.Model):
     """A collection of locations."""
 
     name = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.name
