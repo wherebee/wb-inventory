12cd58fe0d78ef90e91f22db13c3e3e503bbe6b3
Matthew Scott <gldnspud@gmail.com>
2011-05-22 21:04:18 -0700

    First draft of schema.
    Does not yet cover transactions or serial/lot tracking.

--- wbinventory/models.py
+++ wbinventory/models.py
@@ -1,3 +1,126 @@
 from django.db import models
 
-# Create your models here.
+from wbinventory.settings import (
+    CATEGORY_MODEL,
+    CURRENCY_MODEL,
+    ITEM_MODEL,
+    LOCATION_MODEL,
+    THIRD_PARTY_MODEL,
+    UOM_MODEL,
+    PRICE_DECIMAL_PLACES,
+    PRICE_MAX_DIGITS,
+    QUANTITY_DECIMAL_PLACES,
+    QUANTITY_MAX_DIGITS,
+)
+
+
+def price_field(**kwargs):
+    """Create a DecimalField for a price with pre-set decimal places and max digits."""
+    return models.DecimalField(decimal_places=PRICE_DECIMAL_PLACES, max_digits=PRICE_MAX_DIGITS, **kwargs)
+
+
+def quantity_field(**kwargs):
+    """Create a DecimalField for a quantity with pre-set decimal places and max digits."""
+    return models.DecimalField(decimal_places=QUANTITY_DECIMAL_PLACES, max_digits=QUANTITY_MAX_DIGITS, **kwargs)
+
+
+class Category(models.Model):
+    """A categorization of items."""
+
+    name = models.CharField(max_length=200)
+
+
+class Currency(models.Model):
+    """A type of currency used in the purchase or sale of items."""
+
+    code = models.CharField(max_length=10)
+    name = models.CharField(max_length=50)
+    symbol = models.CharField(max_length=10)
+
+
+class Item(models.Model):
+    """A type of item being controlled."""
+
+    number = models.CharField(max_length=100)
+    description = models.TextField(blank=True, default='')
+    default_uom = models.ForeignKey(UOM_MODEL)
+    default_location = models.ForeignKey(LOCATION_MODEL)
+    reorder_quantity = quantity_field(null=True, default=None)
+    target_quantity = quantity_field(null=True, default=None)
+
+    categories = models.ManyToManyField(CATEGORY_MODEL, related_name='items', through='ItemCategory')
+    suppliers = models.ManyToManyField(THIRD_PARTY_MODEL, related_name='items', through='ItemSupplier')
+
+
+class ItemCategory(models.Model):
+    """A categorization of an item."""
+
+    item = models.ForeignKey(ITEM_MODEL)
+    category = models.ForeignKey(CATEGORY_MODEL)
+
+
+class ItemLocation(models.Model):
+    """Location containing a certain quantity of an item."""
+
+    item = models.ForeignKey(ITEM_MODEL)
+    location = models.ForeignKey(LOCATION_MODEL)
+    uom = models.ForeignKey(UOM_MODEL)
+    quantity = quantity_field(default='0')
+
+
+class ItemSupplier(models.Model):
+    """A third party that supplies an item."""
+
+    item = models.ForeignKey(ITEM_MODEL)
+    third_party = models.ForeignKey(THIRD_PARTY_MODEL)
+    priority = models.IntegerField(default=1)  # lower value == higher priority
+    notes = models.TextField(blank=True, default='')
+
+
+class ItemPrice(models.Model):
+    """The price expected to pay a third party for the supply of a certain quantity of an item."""
+
+    item = models.ForeignKey(ITEM_MODEL)
+    third_party = models.ForeignKey(THIRD_PARTY_MODEL)
+    uom = models.ForeignKey(UOM_MODEL)
+    quantity = quantity_field(default='0')
+    currency = models.ForeignKey(CURRENCY_MODEL)
+    price = price_field(default='0')
+
+
+class Location(models.Model):
+    """An area where items are kept."""
+
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, default='')
+    notes = models.TextField(blank=True, default='')
+    zone = models.ForeignKey('Zone', null=True)
+    third_party = models.ForeignKey(THIRD_PARTY_MODEL, null=True)
+
+
+class ThirdParty(models.Model):
+    """The default model representing a third party."""
+
+    name = models.CharField(max_length=200)
+    contact_name = models.CharField(max_length=200, blank=True, default='')
+    phone_number = models.CharField(max_length=50, blank=True, default='')
+    fax_number = models.CharField(max_length=50, blank=True, default='')
+    email_address = models.EmailField(blank=True, default='')
+    website_url = models.URLField(blank=True, default='')
+    notes = models.TextField(blank=True, default='')
+    is_client = models.BooleanField(default=False)
+    is_supplier = models.BooleanField(default=False)
+    is_manufacturer = models.BooleanField(default=False)
+
+
+class UnitOfMeasure(models.Model):
+    """A unit of measure, used descriptively and for conversion."""
+
+    name = models.CharField(max_length=50)
+    default = models.BooleanField(default=False)
+
+
+class Zone(models.Model):
+    """A collection of locations."""
+
+    name = models.CharField(max_length=200)
---
+++ wbinventory/settings.py
@@ -0,0 +1,17 @@
+"""Django settings applicable to wbinventory."""
+
+from django.conf import settings
+
+
+CATEGORY_MODEL = getattr(settings, 'WBINVENTORY_CATEGORY_MODEL', 'wbinventory.Category')
+CURRENCY_MODEL = getattr(settings, 'WBINVENTORY_CURRENCY_MODEL', 'wbinventory.Currency')
+ITEM_MODEL = getattr(settings, 'WBINVENTORY_ITEM_MODEL', 'wbinventory.Item')
+LOCATION_MODEL = getattr(settings, 'WBINVENTORY_LOCATION_MODEL', 'wbinventory.Location')
+THIRD_PARTY_MODEL = getattr(settings, 'WBINVENTORY_THIRD_PARTY_MODEL', 'wbinventory.ThirdParty')
+UOM_MODEL = getattr(settings, 'WBINVENTORY_UOM_MODEL', 'wbinventory.UnitOfMeasure')
+
+PRICE_DECIMAL_PLACES = getattr(settings, 'WBINVENTORY_QUANTITY_DECIMAL_PLACES', 4)
+PRICE_MAX_DIGITS = getattr(settings, 'WBINVENTORY_QUANTITY_MAX_DIGITS', 16)
+
+QUANTITY_DECIMAL_PLACES = getattr(settings, 'WBINVENTORY_QUANTITY_DECIMAL_PLACES', 6)
+QUANTITY_MAX_DIGITS = getattr(settings, 'WBINVENTORY_QUANTITY_MAX_DIGITS', 16)
