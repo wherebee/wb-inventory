84677fdf0d5e08bfced4f4fbb20412f39485a30b
Matthew Scott <gldnspud@gmail.com>
2011-06-08 21:25:22 -0700

    Include an empty index view at the root URL of the app.
    Intended to be guarded by restricting access to those users who have a certain permission.

---
+++ wbinventory/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import patterns, url
+
+
+urlpatterns = patterns('wbinventory.views',
+    url(regex=  r'$^',
+        name=   'wbinventory_index',
+        view=   'index',
+    ),
+)
