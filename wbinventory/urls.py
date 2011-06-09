80bd7337709c258b0339937db7ebc2bab8f3365a
Matthew Scott <gldnspud@gmail.com>
2011-06-08 22:49:04 -0700

    Correct regex in root URL.

--- wbinventory/urls.py
+++ wbinventory/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import patterns, url
 
 
 urlpatterns = patterns('wbinventory.views',
-    url(regex=  r'$^',
+    url(regex=  r'^$',
         name=   'wbinventory_index',
         view=   'index',
     ),
