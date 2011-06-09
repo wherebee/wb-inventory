29ade647cb464c77c09e1703a4a84232ad89fed5
Matthew Scott <gldnspud@gmail.com>
2011-06-08 22:49:22 -0700

    Deny access to index view if user does not have permission.

--- wbinventory/views.py
+++ wbinventory/views.py
@@ -1,5 +1,8 @@
+from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse
 
 
 def index(request):
+    if not request.user.has_module_perms('wbinventory'):
+        raise PermissionDenied()
     return HttpResponse('')
