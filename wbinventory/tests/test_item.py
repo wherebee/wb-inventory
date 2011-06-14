a598f63289154b9d85aadfcbd3c62c3c02def992
Matthew Scott <gldnspud@gmail.com>
2011-06-14 13:01:04 -0700

    Test item detail edit form: success, failure from empty number, failure from duplicate number - closes #9

--- wbinventory/tests/test_item.py
+++ wbinventory/tests/test_item.py
@@ -19,3 +19,21 @@ class ItemTest(BaseTest):
         response = c.get('/wbinventory/items/2/')
         self.assertIn('67890', response.content)
         self.assertIn('six seven eight nine zero', response.content)
+
+    def test_edit_detail_success(self):
+        c = self.logged_in_client()
+        response = c.post('/wbinventory/items/1/edit/', {'number': '6-78901', 'name': 'another name'}, follow=True)
+        self.assertIn('6-78901', response.content)
+        self.assertIn('another name', response.content)
+        self.assertNotIn('12345', response.content)
+        self.assertNotIn('one two three four five', response.content)
+
+    def test_edit_detail_failure_empty_number(self):
+        c = self.logged_in_client()
+        response = c.post('/wbinventory/items/1/edit/', {'number': '', 'name': 'another name'}, follow=True)
+        self.assertIn('This field is required.', response.content)
+
+    def test_edit_detail_failure_duplicate(self):
+        c = self.logged_in_client()
+        response = c.post('/wbinventory/items/1/edit/', {'number': '67890', 'name': 'another name'}, follow=True)
+        self.assertIn('Item with this Number already exists.', response.content)
