54f11638284dbd8ea44632f3bb2dc32ddeb5bc05
Matthew Scott <gldnspud@gmail.com>
2011-06-16 13:37:45 -0700

    Fixture and skeleton test class for ItemTransaction tests. - #21 #22 #23 #24

--- wbinventory/tests/__init__.py
+++ wbinventory/tests/__init__.py
@@ -1,3 +1,4 @@
 # Import tests from submodules.
+from .test_models_itemtransaction import *
 from .test_views_item import *
 from .test_views_sitesearch import *
---
+++ wbinventory/tests/test_models_itemtransaction.py
@@ -0,0 +1,8 @@
+from wbinventory.tests.base import BaseTest
+
+
+class ModelsItemTransactionTest(BaseTest):
+
+    fixtures = [
+        'wbinventory_test_models_itemtransaction.json',
+    ]
